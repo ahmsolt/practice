50f300dac7c9d3ae40ff9226cfa4ec23c0775c73
ahmsolt <ahm.solt@gmail.com>
2024-10-24 12:52:18 +0330

    x1

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
 def blog_view(request):
-    return render (request,'blog/blog-home.html')
-def blog_single(request):
-    return render (request,'blog/blog-single.html')
+    post = Post.objects.all()
+    context = {'post':post}
+    return render (request,'blog/blog-home.html',context)
+def blog_single(request,pid):
+    x1 = get_object_or_404(Post,pk=pid,status=1)
+    context = {'x1':x1}
+    return render (request,'blog/blog-single.html',context)
