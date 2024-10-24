44cf292a6cbf3d245547c5d3b4ba38b25564f07b
ahmsolt <ahm.solt@gmail.com>
2024-10-24 09:21:24 +0330

    Initial commit

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from website.views import *
+from django.urls import path 
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact')
+    
+]
